c7052876bc20104119a60924f14935e5aa269f05
eggsandwich-dev <cpitre1@uncc.edu>
2024-04-27 19:07:07 -0400

    minor fixes

--- src/Python-NLP/model.py
+++ src/Python-NLP/model.py
@@ -1,12 +1,13 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 # Load pretrained GPT-2 model and tokenizer
-model_name = "gpt2-large"
+model_name = "gpt2"
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
 
-def generate_reponse(prompt: str) -> str:
+def generate_response(prompt: str) -> str:
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
+
     # Generate text based on the prompt
     output = model.generate(input_ids, 
                             length_penalty=0.5, 
@@ -18,6 +19,7 @@ def generate_reponse(prompt: str) -> str:
                             do_sample=True,
                             top_k=20,
                             early_stopping=True,
+                            pad_token_id=tokenizer.eos_token_id
                             )
     # Decode and print the generated text
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
