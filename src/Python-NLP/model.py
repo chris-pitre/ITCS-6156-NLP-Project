18e85279c51998bbad78368d18740cae7ac1999a
chris-pitre <c.tophanp3@gmail.com>
2024-04-27 18:01:26 -0400

    tweaked generation parameters

--- src/Python-NLP/model.py
+++ src/Python-NLP/model.py
@@ -1,14 +1,24 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 # Load pretrained GPT-2 model and tokenizer
-model_name = "gpt2"
+model_name = "gpt2-large"
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
 
 def generate_reponse(prompt: str) -> str:
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
     # Generate text based on the prompt
-    output = model.generate(input_ids, max_length=50, no_repeat_ngram_size=12)
+    output = model.generate(input_ids, 
+                            length_penalty=0.5, 
+                            num_beams=4, 
+                            max_length=50, 
+                            no_repeat_ngram_size=2, 
+                            temperature=0.85, 
+                            repetition_penalty=1.5, 
+                            do_sample=True,
+                            top_k=20,
+                            early_stopping=True,
+                            )
     # Decode and print the generated text
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     return generated_text
